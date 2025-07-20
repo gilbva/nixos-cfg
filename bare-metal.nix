f4d3a41faa471487cf9ff53635b8c680535497ba
Gilberto José Vento Alvarez <gilberto.vento@gmail.com>
2025-07-19 23:41:20 -0400

    Update bare-metal.nix

--- bare-metal.nix
+++ bare-metal.nix
@@ -15,7 +15,7 @@
   boot.loader.grub.device = "/dev/nvme1n1p2";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "workstation.gilbva.me"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
