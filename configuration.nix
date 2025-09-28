85b674bc52c7101c4763957bf630a317cbf5d283
gilberto <gilberto.vento@gmail.com>
2025-09-27 20:22:37 -0400

    updating configuration

--- configuration.nix
+++ configuration.nix
@@ -180,6 +180,8 @@
     wpsoffice-cn
     opentelemetry-collector
     cdrtools
+    dvdplusrwtools
+    libisoburn
     # dnsmasq
     # opera
     # dotnet-sdk_10
@@ -199,7 +201,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
